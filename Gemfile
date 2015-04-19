1e1db0da7dbdf4c905dc4ed2b2f89d7b16bff013
Edgar Pabon <edgar.pabon@gmail.com>
2015-04-19 14:11:46 -0500

    Added Devise for User Sign Up

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+# Use Figaro for handling sensitive data using environmental variables
+gem 'figaro', '1.0'
 # Use Bootstrap for CSS
 gem 'bootstrap-sass'
 # Use SQLite3 for Database in development and Rails 12 Factor for deployment on Heroku
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
+
+  devise_for :users
+  resources :users, only: [:update, :show]
 
   root to: 'welcome#index'
 
-  devise_for :users
+end
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -60,4 +66,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
