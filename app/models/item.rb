36e643c150ac1b5a5eea04ede466a2525dc26fcd
Edgar Pabon <edgar.pabon@gmail.com>
2015-04-26 18:26:57 -0500

    Revised algorithm for calculating days remaining

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ActiveRecord::Base
   default_scope { order('updated_at DESC') }
 
   def days_left
-    7 - (DateTime.now.to_date - created_at.to_date).to_i
+    days_of_existence = (DateTime.now.to_date - created_at.to_date).to_i
+    days_of_existence = 0 if days_of_existence <= -1
+    return 7 - days_of_existence
   end
 end
